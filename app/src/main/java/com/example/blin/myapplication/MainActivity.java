d1c9653f5de7bdbe4eb12e278aa6d60de2f1d7a4
blin <bennylin@outlook.com>
2015-01-12 16:05:09 +0800

    test

--- app/src/main/java/com/example/blin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/blin/myapplication/MainActivity.java
@@ -2,29 +2,66 @@ package com.example.blin.myapplication;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+private boolean Flag=false;
+    public void buttontest(View Source) throws IOException {
+       Button bt = (Button)findViewById(R.id.BT1);
+        if(Flag==false) {
+            bt.setBackgroundColor(Color.rgb(0, 122, 0));
+            SwitchPict("2.JPG");
+            Flag=true;
+        }
+        else
+        {
+            bt.setBackgroundColor(Color.CYAN);
+            SwitchPict("benny1.JPG");
+            Flag=false;
 
+        }
+
+    }
+    public void SwitchPict(String s)
+    {
+
+        try {
+            ImageView jpgView1 = (ImageView) findViewById(R.id.jpgview);
+            String myJpgPath = Environment.getExternalStorageDirectory().getPath() + "/"+s;
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 2;
+            Bitmap bm = BitmapFactory.decodeFile(myJpgPath, options);
+            jpgView1.setImageBitmap(bm);
+        }
+        catch(Exception  e)
+        {
+            System.err.print(e.toString());
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView jpgView = (ImageView)findViewById(R.id.jpgview);
 
 
-
+          ImageView jpgView = (ImageView)findViewById(R.id.jpgview);
         String myJpgPath = Environment.getExternalStorageDirectory().getPath()+ "/benny1.JPG";
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = 2;
         Bitmap bm = BitmapFactory.decodeFile(myJpgPath, options);
         jpgView.setImageBitmap(bm);
+
     }
 
 
